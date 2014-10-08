9077a8e526940beec0fc096e0cce467c8185b542
Ben Norris <ben@bsn.io>
2014-10-07 18:14:42 -0600

    Animated a blue square around the edge of the screen

--- AnimationTest/ATViewController.m
+++ AnimationTest/ATViewController.m
@@ -8,6 +8,9 @@
 
 #import "ATViewController.h"
 
+static CGFloat const length = 50.0;
+static CGFloat const margin = 10.0;
+
 @interface ATViewController ()
 
 @end
@@ -18,6 +21,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *blueSquare = [[UIView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + margin, self.view.bounds.origin.y + margin * 2, length, length)];
+    blueSquare.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:blueSquare];
+    
+    // Path animation
+    
+    
+    CGRect boundingRect = CGRectMake(0, 0, self.view.bounds.size.width - margin * 2 - length, self.view.bounds.size.height - margin * 3 - length);
+
+    CAKeyframeAnimation *orbit = [CAKeyframeAnimation animation];
+    orbit.keyPath = @"position";
+    orbit.path = CFAutorelease(CGPathCreateWithRect(boundingRect, NULL));
+//    orbit.path = CFAutorelease(CGPathCreateWithEllipseInRect(boundingRect, NULL));
+    orbit.duration = 4;
+    orbit.additive = YES;
+    orbit.repeatCount = HUGE_VALF;
+    orbit.calculationMode = kCAAnimationPaced;
+    orbit.rotationMode = kCAAnimationRotateAuto;
+
+    [blueSquare.layer addAnimation:orbit forKey:@"orbit"];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
